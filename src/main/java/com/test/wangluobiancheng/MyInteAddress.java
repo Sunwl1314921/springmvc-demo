94998b906e4b994b56b36a839a2c20663e034953
sunwenlong <644013561@qq.com>
2018-12-21 14:59:36 +0800

    spring aop简单理解

---
+++ src/main/java/com/test/wangluobiancheng/MyInteAddress.java
@@ -0,0 +1,20 @@
+package com.test.wangluobiancheng;
+import java.net.InetAddress;
+
+/**
+ * @Author ：SunWenLong
+ * @Date ：2018/12/20
+ */
+public class MyInteAddress {
+    public static void main(String[] args) throws  Exception{
+        InetAddress inetAddress = InetAddress.getByName("ytx5.com");
+        System.out.println(inetAddress);
+        //两个方法
+        System.out.println(inetAddress.getHostName());
+        System.out.println(inetAddress.getHostAddress());
+        //获取本机的IP地址
+        InetAddress inetLocal = InetAddress.getLocalHost();
+        System.out.println(inetLocal);
+    }
+
+}
